dffb694b2779ac4a4e74226d4509a5ce16f8e5ff
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-01-31 22:20:45 -0300

    Created Project

---
+++ src/app/forms-comp/forms-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './forms-comp.component.html',
+  styleUrl: './forms-comp.component.css'
+})
+export class FormsCompComponent {
+
+}
